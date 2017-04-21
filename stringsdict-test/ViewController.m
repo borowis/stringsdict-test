26c8f287b76fd962638576cb4d0eea8d50152230
Borys Zibrov <zebrahere@mail.ru>
2017-04-21 14:24:00 +0300

    initial testing

--- stringsdict-test/ViewController.m
+++ stringsdict-test/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    @property (weak, nonatomic) IBOutlet UILabel *_test_label;
 @end
 
 @implementation ViewController
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    __test_label.text = [NSString stringWithFormat:NSLocalizedString(@"geese.landed.ct", nil), 1, 2];
 }
 
 
